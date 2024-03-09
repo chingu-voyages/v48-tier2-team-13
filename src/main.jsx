2d2bd6947c18011b3fa6cbe0eda7af36dcd72bac
stefanbrkic1 <stefanbrkicdzn.contact@gmail.com>
2024-03-09 23:28:50 +0100

    Code cleanup

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode> Removed strict mode for testing purposes for GeoCode API which has a free tier limit of 
+  // <React.StrictMode> Removed strict mode for testing purposes for GeoCode API which has a free tier limit of
   //1 request per second
-    <App />
+  <App />
   // </React.StrictMode>
 );
--- src/services/GeocodingAPI/geocodingApi.js
+++ src/services/GeocodingAPI/geocodingApi.js
@@ -1,16 +1,18 @@
-import retrieveCountryGeoCoordinates from "../../utils/retrieveCountryGeoCoordinates"
+import retrieveCountryGeoCoordinates from "../../utils/retrieveCountryGeoCoordinates";
 
-async function httpGetGeoCoordinates(countryName){
-    try{
-        const response= await fetch("https://api.geoapify.com/v1/geocode/search?country="+ countryName+ "&api_key="+ import.meta.env.VITE_GEOAPIFY_API_KEY)
-        const data= await response.json()
-        return retrieveCountryGeoCoordinates(data)
-    
-    } catch (error) {
-        console.log("There was an error fetching your request.")
-
-    }
-
+async function httpGetGeoCoordinates(countryName) {
+  try {
+    const response = await fetch(
+      "https://api.geoapify.com/v1/geocode/search?country=" +
+        countryName +
+        "&api_key=" +
+        import.meta.env.VITE_GEOAPIFY_API_KEY
+    );
+    const data = await response.json();
+    return retrieveCountryGeoCoordinates(data);
+  } catch (error) {
+    console.log("There was an error fetching your request.");
+  }
 }
 
-export default httpGetGeoCoordinates
+export default httpGetGeoCoordinates;
