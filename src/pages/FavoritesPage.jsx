c85515594b4098642fe19f299282303f5ef71486
stefanbrkic1 <stefanbrkicdzn.contact@gmail.com>
2024-03-30 19:05:01 +0100

    Favorites Page Corrections

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -1,10 +1,10 @@
 //React imports
-import { useContext} from "react";
+import { useContext } from "react";
 
 // Component Imports
 import Navbar from "../components/Navbar";
-import SearchItemPreview from '../components/SearchItemPreview'
-
+import SearchItemPreview from "../components/SearchItemPreview";
+import Footer from "../components/Footer";
 
 //Id generator
 import { v4 as uuidv4 } from "uuid";
@@ -13,37 +13,44 @@ import { AppContext } from "../App";
 const FAVORITES_PAGE = "FAVORITES_PAGE";
 
 function FavoritesPage() {
-  const {favorites} = useContext(AppContext)
+  const { favorites } = useContext(AppContext);
 
   return (
     <>
       <Navbar activePage={FAVORITES_PAGE} />
-      <section className="pt-[25%] lg:pt-[8%] lg:mb-[5%] mb-[10%] text-text-light w-screen text-center">
-        <h1 className="text-3xl font-bold text-text-light mb-[1%]">Favorites</h1>
-        <h1 className="font-xs text-xs">
+      <section className="mb-[85px] mt-[85px] sm:mt-[100px] bg-bg-primary flex flex-col items-center text-text-light">
+        <h1 className="font-black text-[37px] md:text-[54px] xl:text-[60px]">
+          Favorites
+        </h1>
+        <h1 className="text-[15px] md:text-[16px] xl:text-[17px] text-center">
           Here is the list of dinosaurs you saved as favorites.
         </h1>
-        </section>
-       
-        <div className="container bg-bg-secondary pb-[10%]">
-        <div className="text-text-light text-lg font-bold lg:py-[2%] py-[5%]">Favorite Dinosaurs <span>({favorites.length})</span></div>
-        <div >
-        {favorites.length >= 1 ? (
-            <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-5">
-              {favorites.map((key) => (
-                  <SearchItemPreview 
-                   key={uuidv4()} 
-                   dinosaurItem={JSON.parse(localStorage.getItem(key))}></SearchItemPreview>
-                  
-              ))}
-            </div>
-          ) : (
-            <h2 className="mt-0 text-text-light">You have no favorite dinosaurs saved to your list.</h2>
-          )}
+      </section>
 
+      <div className="bg-bg-secondary">
+        <div className="container bg-bg-secondary pb-[10%]">
+          <div className="text-text-light text-2xl font-bold pt-[40px] pb-2">
+            Favorite Dinosaurs <span>({favorites.length})</span>
+          </div>
+          <div>
+            {favorites.length >= 1 ? (
+              <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-5 mt-5">
+                {favorites.map((key) => (
+                  <SearchItemPreview
+                    key={uuidv4()}
+                    dinosaurItem={JSON.parse(localStorage.getItem(key))}
+                  ></SearchItemPreview>
+                ))}
+              </div>
+            ) : (
+              <h2 className="mt-0 text-text-light">
+                You have no favorite dinosaurs saved to your list.
+              </h2>
+            )}
+          </div>
         </div>
-          
-        </div>
+      </div>
+      <Footer />
     </>
   );
 }
