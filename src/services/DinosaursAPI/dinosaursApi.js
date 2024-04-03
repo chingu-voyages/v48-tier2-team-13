818da670ceb65f677269b6825e10d4f36bd81533
stefanbrkic1 <stefanbrkicdzn.contact@gmail.com>
2024-04-03 15:20:22 +0200

    Removed Log

--- src/services/DinosaursAPI/dinosaursApi.js
+++ src/services/DinosaursAPI/dinosaursApi.js
@@ -7,7 +7,6 @@ async function fetchDinosaursData() {
       throw new Error("Error: Service error");
     }
     const data = await response.json();
-    console.log(data);
     return data;
   } catch (error) {
     console.error(error);
